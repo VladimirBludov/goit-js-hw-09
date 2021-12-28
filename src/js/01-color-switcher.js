e699c65602f5c7f75617e72be00aacd3c198626f
Vladimir Bludov <vladimir.bloodov@gmail.com>
2021-12-28 10:19:30 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.body,
+};
 
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', onClickBtnStart);
+
+refs.btnStop.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart () {
+  if (intervalId) return;
+
+  startChangingBodyBackgroundColor();
+
+  toggleButtonsAttributeDisable();
+}
+
+function onClickBtnStop () {
+  stopChangingBodyBackgroundColor();
+}
+
+function startChangingBodyBackgroundColor (arguments) {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangingBodyBackgroundColor (arguments) {
+  clearInterval(intervalId);
+  intervalId = null;
+  toggleButtonsAttributeDisable();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function toggleButtonsAttributeDisable() {
+  refs.btnStart.toggleAttribute('disabled');
+  refs.btnStop.toggleAttribute('disabled');
+}
