970c71fda52cbb047603ed8c9051dba619060890
Vladimir Bludov <vladimir.bloodov@gmail.com>
2021-12-28 17:49:49 +0200

    save

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+  let delay = Number(formEl.elements.delay.value);
+  const step = Number(formEl.elements.step.value);
+  const amount = Number(formEl.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    if (i !== 0) {
+      delay += step;
+    }
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return Promise.resolve({ position, delay });
   } else {
-    // Reject
+    return Promise.reject({ position, delay });
   }
 }
